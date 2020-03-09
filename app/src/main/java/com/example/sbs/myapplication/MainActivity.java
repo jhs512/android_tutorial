2a5c284b384e0cdc38351f14185fabc45641071f
jhs512 <jangka512@gmail.com>
2020-03-09 22:21:09 +0900

    ObservableField, 양방향 바인딩

--- app/src/main/java/com/example/sbs/myapplication/MainActivity.java
+++ app/src/main/java/com/example/sbs/myapplication/MainActivity.java
@@ -1,14 +1,22 @@
 package com.example.sbs.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+import androidx.databinding.ObservableField;
 
 import android.os.Bundle;
+
+import com.example.sbs.myapplication.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        binding.setLifecycleOwner(this);
+
+        ObservableField<String> obfInputNewTodo = new ObservableField<>("안녕");
+        binding.setInputNewTodo(obfInputNewTodo);
     }
 }
